6e039c309fccac41625def4d0b3a29f72701fd7b
yw981 <yw981@sina.com>
2019-08-16 22:03:28 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Adversarial Robustness Toolbox examples
+
+[cifar_adversarial_training.py](cifar_adversarial_training.py)
+Trains a convolutional neural network on the CIFAR-10 dataset, then generates adversarial images using the
+DeepFool attack and retrains the network on the training set augmented with the adversarial images.
+
+[data_augmentation.py](data_augmentation.py)
+This example shows how to use ART and Keras to perform adversarial training using data generators for CIFAR-10.
+
+[mnist_cnn_fgsm.py](mnist_cnn_fgsm.py)
+Trains a convolutional neural network on MNIST, then crafts FGSM attack examples on it.
+
+[mnist_transferability.py](mnist_transferability.py)
+Trains a convolutional neural network on the MNIST dataset using the Keras backend, then generates adversarial images using DeepFool
+and uses them to attack a convolutional neural network trained on MNIST using TensorFlow. This is to show how to perform a
+black-box attack: the attack never has access to the parameters of the TensorFlow model.
+
+[mnist_poison_detection.py](mnist_poison_detection.py)
+Generates a backdoor for MNIST dataset, then trains a convolutional neural network on the poisoned dataset and runs activation defence to find poison.
+
+## Using Jupyter Notebooks to run ART
+
+We have also created a sample Jupyter notebook in [Fabric for Deep Learning](https://github.com/IBM/FfDL) repository which shows you how to launch training using FfDL, and then invoke ART to find model vulnerabilities. Fabric for Deep Learning (FfDL, pronounced fiddle) is a Deep Learning Platform offering TensorFlow, Caffe, PyTorch etc. as a Service on Kubernetes
+
+Find more details in the [code pattern here](https://developer.ibm.com/code/patterns/integrate-adversarial-attacks-model-training-pipeline/).
---
+++ mnist_transferability.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""Trains a CNN on the MNIST dataset using the Keras backend, then generates adversarial images using DeepFool
+and uses them to attack a CNN trained on MNIST using TensorFlow. This is to show how to perform a
+black-box attack: the attack never has access to the parameters of the TensorFlow model.
+"""
+from __future__ import absolute_import, division, print_function
+
+import sys
+from os.path import abspath
+
+sys.path.append(abspath('.'))
+
+import keras
+import keras.backend as k
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+import numpy as np
+import tensorflow as tf
+
+from art.attacks.deepfool import DeepFool
+from art.classifiers import KerasClassifier, TFClassifier
+from art.utils import load_mnist
+
+
+def cnn_mnist_tf(input_shape):
+    labels_tf = tf.placeholder(tf.float32, [None, 10])
+    inputs_tf = tf.placeholder(tf.float32, [None] + list(input_shape))
+
+    # Define the tensorflow graph
+    conv = tf.layers.conv2d(inputs_tf, 4, 5, activation=tf.nn.relu)
+    conv = tf.layers.max_pooling2d(conv, 2, 2)
+    fc = tf.contrib.layers.flatten(conv)
+
+    # Logits layer
+    logits = tf.layers.dense(fc, 10)
+
+    # Train operator
+    loss = tf.reduce_mean(tf.losses.softmax_cross_entropy(logits=logits, onehot_labels=labels_tf))
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
+    train_tf = optimizer.minimize(loss)
+
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+
+    classifier = TFClassifier(inputs_tf, logits, clip_values=(0, 1), loss=loss, train=train_tf, output_ph=labels_tf,
+                              sess=sess)
+
+    return classifier
+
+
+def cnn_mnist_k(input_shape):
+    # Create simple CNN
+    model = Sequential()
+    model.add(Conv2D(4, kernel_size=(5, 5), activation='relu', input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(10, activation='softmax'))
+
+    model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(lr=0.01),
+                  metrics=['accuracy'])
+
+    classifier = KerasClassifier(model, clip_values=(0, 1), use_logits=False)
+    return classifier
+
+
+# Get session
+session = tf.Session()
+k.set_session(session)
+
+# Read MNIST dataset
+(x_train, y_train), (x_test, y_test), min_, max_ = load_mnist()
+
+# Construct and train a convolutional neural network on MNIST using Keras
+source = cnn_mnist_k(x_train.shape[1:])
+source.fit(x_train, y_train, nb_epochs=5, batch_size=128)
+
+# Craft adversarial samples with DeepFool
+adv_crafter = DeepFool(source)
+x_train_adv = adv_crafter.generate(x_train)
+x_test_adv = adv_crafter.generate(x_test)
+
+# Construct and train a convolutional neural network
+target = cnn_mnist_tf(x_train.shape[1:])
+target.fit(x_train, y_train, nb_epochs=5, batch_size=128)
+
+# Evaluate the CNN on the adversarial samples
+preds = target.predict(x_test_adv)
+acc = np.sum(np.equal(np.argmax(preds, axis=1), np.argmax(y_test, axis=1))) / y_test.shape[0]
+print("\nAccuracy on adversarial samples: %.2f%%" % (acc * 100))
