06f4e2364afd39f91d87088724d6d41ccc36e4d5
Tony Yang <yw981@sina.com>
2019-08-20 10:40:18 +0800

    keras pretrained

--- mnist_cnn_fgsm.py
+++ mnist_cnn_fgsm.py
@@ -21,33 +21,35 @@ from art.utils import load_dataset
 
 # Create Keras convolutional neural network - basic architecture from Keras examples
 # Source here: https://github.com/keras-team/keras/blob/master/examples/mnist_cnn.py
-k.set_learning_phase(1)
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=x_train.shape[1:]))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(10, activation='softmax'))
-
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-classifier = KerasClassifier(model=model, clip_values=(min_, max_))
-classifier.fit(x_train, y_train, nb_epochs=5, batch_size=128)
-
-# Evaluate the classifier on the test set
-preds = np.argmax(classifier.predict(x_test), axis=1)
-acc = np.sum(preds == np.argmax(y_test, axis=1)) / y_test.shape[0]
-print("\nTest accuracy: %.2f%%" % (acc * 100))
+# k.set_learning_phase(1)
+# model = Sequential()
+# model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=x_train.shape[1:]))
+# model.add(Conv2D(64, (3, 3), activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.25))
+# model.add(Flatten())
+# model.add(Dense(128, activation='relu'))
+# model.add(Dropout(0.5))
+# model.add(Dense(10, activation='softmax'))
+#
+# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+#
+# classifier = KerasClassifier(model=model, clip_values=(min_, max_))
+# classifier.fit(x_train, y_train, nb_epochs=5, batch_size=128)
+#
+# # Evaluate the classifier on the test set
+# preds = np.argmax(classifier.predict(x_test), axis=1)
+# acc = np.sum(preds == np.argmax(y_test, axis=1)) / y_test.shape[0]
+# print("\nTest accuracy: %.2f%%" % (acc * 100))
 
 # Craft adversarial samples with FGSM
 epsilon = .1  # Maximum perturbation
-adv_crafter = FastGradientMethod(classifier, eps=epsilon)
+# adv_crafter = FastGradientMethod(classifier, eps=epsilon)
+adv_crafter = FastGradientMethod(eps=epsilon)
 x_test_adv = adv_crafter.generate(x=x_test)
+print(x_test_adv)
 
 # Evaluate the classifier on the adversarial examples
-preds = np.argmax(classifier.predict(x_test_adv), axis=1)
-acc = np.sum(preds == np.argmax(y_test, axis=1)) / y_test.shape[0]
-print("\nTest accuracy on adversarial sample: %.2f%%" % (acc * 100))
+# preds = np.argmax(classifier.predict(x_test_adv), axis=1)
+# acc = np.sum(preds == np.argmax(y_test, axis=1)) / y_test.shape[0]
+# print("\nTest accuracy on adversarial sample: %.2f%%" % (acc * 100))
